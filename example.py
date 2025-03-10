65e595f3431ae767bdb513454ac2cada8311822f
cchuenchoksan <paopaochofficial@gmail.com>
2025-03-10 16:50:55 +0800

    commit check

--- example.py
+++ example.py
@@ -10,12 +10,12 @@ import numpy as np
 # ipok_rsi.returns_plot()
 # ipok_rsi.drawdown()
 
-ipok_ma = MACrossover("AAPL", "2022-01-01", "2023-12-31", "1D")
-ipok_ma.strategy(5, 50)
-ipok_ma.visualise_strategy()
-ipok_ma.print_performance()
-ipok_ma.returns_plot()
-ipok_ma.drawdown()
+# ipok_ma = MACrossover("AAPL", "2022-01-01", "2023-12-31", "1D")
+# ipok_ma.strategy(5, 50)
+# ipok_ma.visualise_strategy()
+# ipok_ma.print_performance()
+# ipok_ma.returns_plot()
+# ipok_ma.drawdown()
 
 # ipok_adx = ADXDI("AAPL", "2022-01-01", "2023-12-31", "1D")
 # ipok_adx.strategy(25, 20)
